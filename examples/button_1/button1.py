4aee2dd5fc2b6e0ea4acc050335a37ec24e519b8
Vignesh Natarajan <vikiworks.io@gmail.com>
2021-10-28 03:24:04 +0200

    button

---
+++ examples/button_1/button1.py
@@ -0,0 +1,25 @@
+from UI import *
+
+def button_callback():
+    print("hello")
+
+
+root = BEGIN()
+root.title("demo")
+root.dimension(500,500)
+#root.bg('#555')
+root.gotoxy(200,100)
+
+obj = button(root)
+obj.callback(button_callback)
+obj.write("test")
+obj.dimension(200, 50)
+obj.gotoxy(30, 100)
+obj.font_size(12)
+obj.bg('#111')
+obj.fg("white")
+#obj.disable()
+#obj.enable()
+
+END(root)
+
