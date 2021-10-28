fec0a204944044a947750e67c533ef8500ee2bea
Vignesh Natarajan <vikiworks.io@gmail.com>
2021-10-28 03:58:46 +0200

    display area with package

--- examples/display_area1/display_area1.py
+++ examples/display_area1/display_area1.py
@@ -1,163 +1,27 @@
-try:
-    import os
-    import tkinter as tk
-    import tkinter.ttk as ttk
-    from tkinter import filedialog
-    import tkinter.scrolledtext as scrolledtext
-    import tkFont
-except ImportError:
-    import Tkinter as tk
-    import ttk
-    import tkFileDialog as filedialogs
-    import Tkinter.scrolledtext as scrolledtext
-    import tkFont
+from UI import *
+
+def button_callback():
+    print("hello")
 
 
-# System Variables Start
-main=None
-root=None
-# System Variables End
-
-class MAIN:
-    def __init__(self):
-        global main
-        self._TITLE='demo'
-        self._BG_COLOR='#555'
-        self._WIDTH=200
-        self._HEIGHT=200
-        self._DIMENSION='' + str(self._WIDTH) + 'x' + str(self._HEIGHT) + ''
-        main = tk.Tk()
-        self.bg(self._BG_COLOR)
-        self.title(self._TITLE)
-        self.dimension(self._WIDTH, self._HEIGHT)
-        self.disable_resize()
-
-    def dimension(self, w, h):
-        global main
-        self._WIDTH = str(w)
-        self._HEIGHT = str(h)
-        self._DIMENSION = '' + str(self._WIDTH) + 'x' + str(self._HEIGHT) + ''
-        main.geometry(self._DIMENSION)
-        w = main.winfo_reqwidth()
-        h = main.winfo_reqheight()
-        px=int(main.winfo_screenwidth()/3 - w/2)
-        py=int(main.winfo_screenheight()/3 - h/2)
-        main.geometry('+{}+{}'.format(px, py))
-
-    def disable_resize(self):
-        global main
-        main.resizable(False, False)
-
-    def enable_resize(self):
-        global main
-        main.resizable(True, True)
-
-    def bg(self, bg_color):
-        global main
-        _BG_COLOR = bg_color
-        main.configure(bg=_BG_COLOR)
-
-    def title(self, title_string):
-        global main, _TITLE
-        _TITLE = title_string;
-        main.winfo_toplevel().title(_TITLE)
-
-    def gotoxy(self, x, y):
-        global main
-        main.geometry('+{}+{}'.format(x, y))
-
-def BEGIN():
-    global root, main
-    root = MAIN()
-
-def END():
-    global main
-    main.mainloop()
-
-
-class display_area:
-    def __init__(self):
-        global main
-        self.font_type = 'consolas'
-        self.fsize = '12'
-        self.width = 10
-        self.height = 1
-        self.bg_color = "white"
-        self.fg_color = "black"
-        self.border_color="black"
-
-        self.object = tk.scrolledtext.ScrolledText(master=main,
-                                  cursor="arrow",
-                                  bg=self.bg_color,
-                                  undo=True)
-
-        self.dimension(self.width, self.height)
-        self.font(self.font_type)
-        self.font_size(self.fsize)
-        self.enable_text_wrap()
-        self.object.pack()
-
-    def enable_text_wrap(self):
-        self.object.config(wrap=tk.WORD)
-
-    def font(self, font_type):
-        self.font_type = font_type
-        self.object.config(font=(self.font_type, self.fsize))
-
-    def font_size(self, fsize):
-        self.fsize = str(fsize)
-        self.object.config(font=(self.font_type, self.fsize))
-
-    def bg(self, bg_color):
-        self.bg_color = bg_color
-        self.object.configure(bg=self.bg_color)
-
-    def fg(self, fg_color):
-        self.fg_color = fg_color
-        self.object.configure(fg=self.fg_color)
-
-    def gotoxy(self, x, y):
-        self.object.place(x=x,y=y)
-
-    def write(self, string):
-        nstring=string
-        self.object.insert(tk.END, nstring)
-
-    def read(self):
-        return self.object.get('1.0', tk.END)
-
-    def disable(self):
-        self.object.configure(state='disabled')
-
-    def enable(self):
-        self.object.configure(state='normal')
-
-    def dimension(self, w, h):
-        self.width = w
-        self.height = h
-        self.object.config(width=self.width)
-        self.object.config(height=self.height)
-
-
-BEGIN()
-
+root = BEGIN()
 root.title("demo")
 root.dimension(500,500)
-root.bg('#555')
+#root.bg('#555')
 root.gotoxy(200,100)
 
-obj = display_area()
-obj.dimension(52, 20)
-obj.gotoxy(30, 100)
+obj = display_area(root)
+obj.dimension(30, 20)
+obj.gotoxy(120, 100)
 obj.font_size(12)
-obj.write("test 1\n")
-obj.write("test 2\n")
-obj.write("test 3\n")
-obj.bg("white")
-obj.fg("green")
-print obj.read()
+obj.bg('black')
+obj.fg("white")
+obj.write("Line 1\n")
+obj.write("Line 2\n")
+
+print(obj.read())
 #obj.disable()
 #obj.enable()
 
-END()
+END(root)
 
--- examples/input_1/input.py
+++ examples/input_1/input.py
@@ -1,150 +1,24 @@
-try:
-    import os
-    import tkinter as tk
-    import tkinter.ttk as ttk
-    from tkinter import filedialog
-    import tkinter.scrolledtext as scrolledtext
-    import tkFont
-except ImportError:
-    import Tkinter as tk
-    import ttk
-    import tkFileDialog as filedialogs
-    import Tkinter.scrolledtext as scrolledtext
-    import tkFont
+from UI import *
+
+def button_callback():
+    print("hello")
 
 
-# System Variables Start
-main=None
-root=None
-# System Variables End
-
-class MAIN:
-    def __init__(self):
-        global main
-        self._TITLE='demo'
-        self._BG_COLOR='#555'
-        self._WIDTH=200
-        self._HEIGHT=200
-        self._DIMENSION='' + str(self._WIDTH) + 'x' + str(self._HEIGHT) + ''
-        main = tk.Tk()
-        self.bg(self._BG_COLOR)
-        self.title(self._TITLE)
-        self.dimension(self._WIDTH, self._HEIGHT)
-        self.disable_resize()
-
-    def dimension(self, w, h):
-        global main
-        self._WIDTH = str(w)
-        self._HEIGHT = str(h)
-        self._DIMENSION = '' + str(self._WIDTH) + 'x' + str(self._HEIGHT) + ''
-        main.geometry(self._DIMENSION)
-        w = main.winfo_reqwidth()
-        h = main.winfo_reqheight()
-        px=int(main.winfo_screenwidth()/3 - w/2)
-        py=int(main.winfo_screenheight()/3 - h/2)
-        main.geometry('+{}+{}'.format(px, py))
-
-    def disable_resize(self):
-        global main
-        main.resizable(False, False)
-
-    def enable_resize(self):
-        global main
-        main.resizable(True, True)
-
-    def bg(self, bg_color):
-        global main
-        _BG_COLOR = bg_color
-        main.configure(bg=_BG_COLOR)
-
-    def title(self, title_string):
-        global main, _TITLE
-        _TITLE = title_string;
-        main.winfo_toplevel().title(_TITLE)
-
-    def gotoxy(self, x, y):
-        global main
-        main.geometry('+{}+{}'.format(x, y))
-
-def BEGIN():
-    global root, main
-    root = MAIN()
-
-def END():
-    global main
-    main.mainloop()
-
-
-class input_box:
-    def __init__(self):
-        global main
-        self.font_type = 'consolas'
-        self.fsize = '12'
-        self.width = 10
-        self.height = 1
-        self.bg_color = "white"
-        self.fg_color = "black"
-        self.border_color="black"
-        self.object = tk.Entry(main)
-        self.font(self.font_type)
-        self.font_size(self.fsize)
-        self.dimension(self.width, self.height)
-        self.object.pack()
-
-    def font(self, font_type):
-        self.font_type = font_type
-        self.object.config(font=(self.font_type, self.fsize))
-
-    def font_size(self, fsize):
-        self.fsize = str(fsize)
-        self.object.config(font=(self.font_type, self.fsize))
-
-    def bg(self, bg_color):
-        self.bg_color = bg_color
-        self.object.configure(bg=self.bg_color)
-
-    def fg(self, fg_color):
-        self.fg_color = fg_color
-        self.object.configure(fg=self.fg_color)
-
-    def gotoxy(self, x, y):
-        self.object.place(x=x,y=y)
-
-    def write(self, string):
-        nstring=string
-        self.object.insert(tk.END, nstring)
-
-    def read(self):
-        return self.object.get()
-
-    def disable(self):
-        self.object.configure(state='disabled')
-
-    def enable(self):
-        self.object.configure(state='normal')
-
-    def dimension(self, w, h):
-        self.width = w
-        self.object.config(width=self.width)
-
-
-BEGIN()
-
+root = BEGIN()
 root.title("demo")
 root.dimension(500,500)
-root.bg('#555')
+#root.bg('#555')
 root.gotoxy(200,100)
 
-obj = input_box()
-obj.dimension(52, 1)
-obj.gotoxy(30, 100)
+obj = input(root)
+obj.dimension(30, 1)
+obj.gotoxy(120, 100)
 obj.font_size(12)
-obj.write("test")
-obj.bg("red")
-obj.fg("green")
-print obj.read()
+obj.bg('black')
+obj.fg("white")
+obj.write("Input Box")
 #obj.disable()
 #obj.enable()
 
-END()
+END(root)
 
