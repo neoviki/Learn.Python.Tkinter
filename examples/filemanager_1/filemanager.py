80891ea5d7cf0062f9893846f2cb66799e6c6b88
Vignesh Natarajan <vikiworks.io@gmail.com>
2021-10-28 04:48:45 +0200

    file manager with package

--- examples/filemanager_1/filemanager.py
+++ examples/filemanager_1/filemanager.py
@@ -1,15 +1,5 @@
-try:
-    import os
-    import tkinter as tk
-    import tkinter.ttk as ttk
-    from tkinter import filedialog
-except ImportError:
-    import Tkinter as tk
-    import ttk
-    import tkFileDialog as filedialog
-
-TITLE="demo"
-WINDOW_BG='#555'
+from UI import *
+from tkinter import filedialog
 
 FILE_TYPES=[('PDF files', '.pdf'),
             ('JPG files', '.jpg'),
@@ -17,54 +7,41 @@ FILE_TYPES=[('PDF files', '.pdf'),
             ('Py files', '*.py'),
             ('all files', '.*')]
 
-root = None
-style = None
-
-def init():
-    global root, style
-    root = tk.Tk()
-    root.configure(bg=WINDOW_BG)
-    root.resizable(False, False)
-    root.winfo_toplevel().title(TITLE)
-    root.geometry('500x300')
-    width = root.winfo_reqwidth()
-    height = root.winfo_reqheight()
-
-    px=int(root.winfo_screenwidth()/3 - width/2)
-    py=int(root.winfo_screenheight()/3 - height/2)
-    root.geometry('+{}+{}'.format(px, py))
-
-    style = ttk.Style()
-    style.theme_use("clam")
-
-def cb_open_folder():
+def open_folder():
     global root
     rep = filedialog.askdirectory(
-        parent=root,
+        parent=root.object,
         initialdir=os.getcwd())
     print(rep)
 
-def cb_open_file():
+def open_file():
     global root
-    rep = filedialog.askopenfilenames(parent=root, initialdir=os.getcwd(), filetypes=FILE_TYPES)
+    rep = filedialog.askopenfilenames(parent=root.object, initialdir=os.getcwd(), filetypes=FILE_TYPES)
     print(rep[0])
-    #try:
-    #    os.startfile(rep[0])
-    #except IndexError:
-    #    print("No file selected")
 
-def start():
-    global root
-    root.mainloop()
 
-def vw_button(x, y, text, callback ):
-    b = ttk.Button(root, text=text,command=callback)
-    b.place(x=x,y=y)
-    return b
+root = BEGIN()
+root.title("demo")
+root.dimension(500,300)
+#root.bg('#555')
+root.gotoxy(200,100)
 
-init()
+b1 = button(root)
+b1.dimension(60, 5)
+b1.gotoxy(200, 100)
+b1.font_size(12)
+b1.write("Open Folder")
+b1.callback(open_folder)
 
-vw_button(200, 100, "Open Folders", cb_open_folder)
-vw_button(200, 150, "Open Files", cb_open_file)
+b2 = button(root)
+b2.dimension(60, 5)
+b2.gotoxy(200, 150)
+b2.font_size(12)
+b2.write("Open File")
+b2.callback(open_file)
 
-start()
+#obj.disable()
+#obj.enable()
+
+END(root)
+
